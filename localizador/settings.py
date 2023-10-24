6f2131a575029d89e9fbf0c5400e4af6e6ba658d
William Alonzo <williamalonzo4519@gmail.com>
2023-10-23 20:45:09 -0600

    sqlite

--- localizador/settings.py
+++ localizador/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'liststyle',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,8 +45,7 @@ INSTALLED_APPS = [
     'categorias',
     'serviciocliente',
     'import_export',
-    'rangefilter',
-    'liststyle'
+    'rangefilter'
 ]
 
 MIDDLEWARE = [
